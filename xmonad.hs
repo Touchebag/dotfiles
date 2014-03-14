55a8477fca4b055807e26acd08c57da779fea5fe
Touchebag <robin_touche@hotmail.com>
2014-03-14 23:20:34 +0100

    Added some keybindings in xmonad

--- xmonad.hs
+++ xmonad.hs
@@ -1,23 +1,31 @@
+import Data.Map (Map, fromList, union)
+
 import XMonad
+import XMonad.Actions.Navigation2D
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.NoBorders
+import XMonad.Util.Run
 
 main :: IO()
 main = do
    spawn "panel"
    xmonad $ defaultConfig
-      { terminal    = myTerminal
-      , modMask     = myModMask
-      , borderWidth = myBorderWidth
-      , manageHook  = myManageHook
-      , layoutHook  = myLayoutHook
+      { terminal           = myTerminal
+      , focusFollowsMouse  = False
+      , focusedBorderColor = winBorderFocused
+      , normalBorderColor  = winBorderNormal
+      , modMask            = myModKey
+      , keys               = union myKeyMaps . keys defaultConfig
+      , borderWidth        = myBorderWidth
+      , manageHook         = myManageHook
+      , layoutHook         = myLayoutHook
    }
 
 myTerminal :: String
 myTerminal = "xterm"
 
-myModMask :: KeyMask
-myModMask = mod4Mask
+myModKey :: KeyMask
+myModKey = mod4Mask
 
 myBorderWidth :: Dimension
 myBorderWidth = 1
@@ -25,5 +33,26 @@ myBorderWidth = 1
 myManageHook :: ManageHook
 myManageHook = manageDocks
 
+winBorderFocused, winBorderNormal :: String
+winBorderFocused = "#ffffff"
+winBorderNormal = "#333333"
+
 myLayoutHook = avoidStruts $ smartBorders $ tiled ||| Mirror tiled ||| Full
       where tiled = Tall 1 0.03 0.5
+
+--{{{ Keymaps
+myKeyMaps :: Map (KeyMask, KeySym) (X ())
+myKeyMaps = fromList $
+   [ ((myModKey, xK_r), spawn "dmenu_run")
+   , ((myModKey, xK_f), safeSpawnProg myTerminal)
+   , ((myModKey, xK_e), safeSpawnProg "firefox")
+   
+   , ((myModKey, xK_h), windowGo L False)
+   , ((myModKey, xK_l), windowGo R False)
+   , ((myModKey, xK_j), windowGo D False)
+   , ((myModKey, xK_k), windowGo U False)
+   
+   , ((myModKey, xK_q), kill)
+   , ((myModKey, xK_p), spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi")
+   ]
+--}}}
